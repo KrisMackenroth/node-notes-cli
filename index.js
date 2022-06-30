581abe6cf25be7b2587b6c76468a95b3c7bd2c45
Kristopher Mackenroth <Kris.mackenroth@gmail.com>
2022-06-30 21:51:49 +0000

    test.

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
 const json = require('./data.json');
+
 if (process.argv[2] === 'read') {
   for (var x in json.notes) {
     console.log(JSON.stringify(json.notes[x], null, 2));
   }
 }
-json.notes[json.nextId] = JSON.stringify(5, null, 2)
-console.log(json.notes[json.nextId]);
+if (process.argv[2] === 'create') {
+json.notes[json.nextId] = process.argv[3];
+json.nextId++;
 //json.notes[json.nextId] = JSON.stringify(process.argv[2], null, 2)
-fs.writeFile('./data.json', JSON.stringify(json.notes, null, 2), err => {
+fs.writeFile('./data.json', JSON.stringify(json, null, 2), err => {
   if (err) throw err;
 })
+}
